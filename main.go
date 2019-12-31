f3183d6010511e56fdb49becc6b50a9e59d1828e
kakuilan <kakuilan@163.com>
2019-12-31 18:50:55 +0800

    add AsciiJSON test

--- main.go
+++ main.go
@@ -55,6 +55,16 @@ func setupRouter() *gin.Engine {
 		}
 	})
 
+	r.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+
+		// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
 	return r
 }
 
