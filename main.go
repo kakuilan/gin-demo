4ab3862653461173507a863b5833ae0a1d2ce1ba
kakuilan <kakuilan@163.com>
2020-01-01 11:11:56 +0800

    add 使用 SecureJSON 防止 json 劫持

--- main.go
+++ main.go
@@ -175,6 +175,16 @@ func setupRouter() *gin.Engine {
 		})
 	})
 
+	// 使用 SecureJSON 防止 json 劫持。如果给定的结构是数组值，则默认预置 "while(1)," 到响应体。
+	// 你也可以使用自己的 SecureJSON 前缀
+	// r.SecureJsonPrefix(")]}',\n")
+	r.GET("/SecureJSON", func(c *gin.Context) {
+		names := []string{"lena", "austin", "foo","<b>你好，世界！</b>"}
+		// 将输出：while(1);["lena","austin","foo"]
+		c.SecureJSON(http.StatusOK, names)
+	})
+
+
 	return r
 }
 
